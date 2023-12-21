862e853cd74742e98361cdeeb7974b916b7d52d3
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-12-21 12:17:01 +0600

    fixed scss file

--- tamanna-portfolio-update/src/App.js
+++ tamanna-portfolio-update/src/App.js
@@ -1,11 +1,13 @@
 // import logo from './logo.svg';
 // import { TERipple } from "tw-elements-react";
 import './App.css';
+import Main from './layout/Main/Main';
 
 function App() {
   return (
     <div className="App">
      <h1>hello</h1>
+     <Main/>
 
 
     </div>
---
+++ tamanna-portfolio-update/src/layout/Main/Main.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Main.scss'
+
+const Main = () => {
+    return (
+        <div>
+            <div className="content">
+                <h1 className='name'> tamanna </h1>
+            </div>
+            
+        </div>
+    );
+};
+
+export default Main;
