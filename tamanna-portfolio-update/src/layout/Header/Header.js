7413c35dd8ab1924a546589b36983ca5e61a3c75
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-12-21 12:28:20 +0600

    header file declear

---
+++ tamanna-portfolio-update/src/layout/Header/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Header.scss';
+
+const Header = () => {
+    return (
+        <div className='header'>
+            <div className="headerMain">
+                <div className="headerContainer">
+                    
+                </div>
+            </div>
+            
+        </div>
+    );
+};
+
+export default Header;
